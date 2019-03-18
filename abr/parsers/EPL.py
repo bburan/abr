07025b77f917a2ce0308264f94535cc4871d9ce6
Brad Buran <bburan@alum.mit.edu>
2019-03-18 12:01:57 -0700

    Fix EPL parser to work with master

--- abr/parsers/EPL.py
+++ abr/parsers/EPL.py
@@ -1,12 +1,17 @@
-import numpy as np
-import re
 import io
+from pathlib import Path
+import re
+
+import numpy as np
+import pandas as pd
+from scipy import signal
 
 from abr.datatype import ABRWaveform, ABRSeries
 
 
-def load(fname, filter=None, frequencies=None):
-    with io.open(fname, encoding='ISO-8859-1') as f:
+def load(filename, filter=None, frequencies=None):
+    filename = Path(filename)
+    with filename.open(encoding='ISO-8859-1') as f:
         line = f.readline()
         if not line.startswith(':RUN-'):
             raise IOError('Unsupported file format')
@@ -15,9 +20,10 @@ def load(fname, filter=None, frequencies=None):
     p_fs = re.compile('SAMPLE \(.sec\): ([0-9]+)')
     p_freq = re.compile('FREQ: ([0-9\.]+)')
 
+
     abr_window = 8500  # usec
     try:
-        with io.open(fname, encoding='ISO-8859-1') as f:
+        with filename.open(encoding='ISO-8859-1') as f:
             header, data = f.read().split('DATA')
 
             # Extract data from header
@@ -32,19 +38,25 @@ def load(fname, filter=None, frequencies=None):
             data = np.array(data.split()).astype(np.float32)
             data.shape = -1, len(levels)
             data = data.T[:, :cutoff]
+            t = np.arange(data.shape[-1]) / fs * 1e3
+            t = pd.Index(t, name='time')
+
+            if filter is not None:
+                Wn = filter['highpass']/(0.5*fs), filter['lowpass']/(0.5*fs)
+                N = filter['order']
+                b, a = signal.iirfilter(N, Wn)
+                data = signal.filtfilt(b, a, data, axis=-1)
 
             waveforms = []
-            for signal, level in zip(data, levels):
+            for s, level in zip(data, levels):
                 # Checks for a ABR I-O bug that sometimes saves zeroed waveforms
-                if not (signal == 0).all():
-                    # Add new dimension to signal since the updated ABR program
-                    # now takes individual waveforms.
-                    waveform = ABRWaveform(fs, signal[np.newaxis], level,
-                                           filter=filter)
+                if not (s == 0).all():
+                    w = pd.Series(s, index=t)
+                    waveform = ABRWaveform(fs, w, level)
                     waveforms.append(waveform)
 
             series = ABRSeries(waveforms, frequency)
-            series.filename = fname
+            series.filename = filename
             return [series]
 
     except (AttributeError, ValueError):
